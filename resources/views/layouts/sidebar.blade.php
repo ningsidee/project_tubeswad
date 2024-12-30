d19191eecaec9463a5a29952cb2e8fc1d5c25151
rdtyaaa <109738535+ningsidee@users.noreply.github.com>
2024-12-30 22:24:37 +0700

    tirza_progress 3

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -1,5 +1,4 @@
 <nav class="d-flex flex-column align-items-center align-items-sm-start position-fixed bg-body-tertiary start-0 top-0 mb-5 rounded p-3 text-white shadow"
-<nav class="d-flex flex-column align-items-center align-items-sm-start position-fixed bg-body-tertiary start-0 top-0 mb-5 rounded p-3 text-white shadow"
     style="width: 300px; height: 100vh; 
            background: linear-gradient(to bottom, #F3E5F5, #D8BFD8, #5c275e); 
            background-size: cover;
@@ -67,8 +66,8 @@
             </li>
 
             <!-- Health Indicator -->
-            <li class="nav-item {{ Route::is('schedulings.index') ? 'fw-bold' : '' }} mb-3">
-                <a href="{{ route('schedulings.index') }}" class="nav-link d-flex align-items-center"
+            <li class="nav-item {{ Route::is('indikator-kesehatan.index') ? 'fw-bold' : '' }} mb-3">
+                <a href="{{ route('indikator-kesehatan.index') }}" class="nav-link d-flex align-items-center"
                     style="color: #4B0082;">
                     <span class="material-symbols-rounded" style="color: #4B0082;">medical_information</span>
                     <span class="d-none d-sm-inline ms-3">Health Indicator</span>
@@ -76,8 +75,8 @@
             </li>
 
             <!-- Daily Activity -->
-            <li class="nav-item {{ Route::is('schedulings.index') ? 'fw-bold' : '' }} mb-3">
-                <a href="{{ route('schedulings.index') }}" class="nav-link d-flex align-items-center"
+            <li class="nav-item {{ Route::is('aktivitas_harian.index') ? 'fw-bold' : '' }} mb-3">
+                <a href="{{ route('aktivitas_harian.index') }}" class="nav-link d-flex align-items-center"
                     style="color: #4B0082;">
                     <span class="material-symbols-rounded" style="color: #4B0082;">footprint</span>
                     <span class="d-none d-sm-inline ms-3">Daily Activity</span>
@@ -85,8 +84,8 @@
             </li>
 
             <!-- Community -->
-            <li class="nav-item {{ Route::is('schedulings.index') ? 'fw-bold' : '' }} mb-3">
-                <a href="{{ route('schedulings.index') }}" class="nav-link d-flex align-items-center"
+            <li class="nav-item {{ Route::is('communities.index') ? 'fw-bold' : '' }} mb-3">
+                <a href="{{ route('communities.index') }}" class="nav-link d-flex align-items-center"
                     style="color: #4B0082;">
                     <span class="material-symbols-rounded" style="color: #4B0082;">diversity_1</span>
                     <span class="d-none d-sm-inline ms-3">Community</span>
