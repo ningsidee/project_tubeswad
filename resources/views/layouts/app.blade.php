e2e03288ddf4bd99eade4a9f09390e5d2c9c5be3
indahfa1 <indahifa22@gmail.com>
2024-12-31 14:36:10 +0700

    update app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,7 +31,6 @@
             @yield('content')
         </div>
     </div>
-    </div>
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
